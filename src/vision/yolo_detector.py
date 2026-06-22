3551ba5931f47c73089c14868d693bc1e0b936d7
Taha <tahaasbzdeh@gmail.com>
2026-06-22 17:51:02 +0330

    push changes

--- src/vision/yolo_detector.py
+++ src/vision/yolo_detector.py
@@ -1,1 +1,7 @@
+from ultralytics import YOLO # type: ignore
 
+model = YOLO('yolo26n-pose.yaml')
+model = YOLO('yolo26n-pose.pt')
+model = YOLO('yolo26n-pose.yaml').load('yolo26n-pose.pt')
+
+res = model.train(data = 'coco8-pose.yaml',epochs = 100, imgsz=640)
